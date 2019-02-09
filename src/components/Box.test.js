ba2e526c396b0ce58dee590396530e80236cc563
Erick Costa Cavalcante <erickbelfort@Ericks-MacBook-Pro.local>
2019-02-09 13:56:06 -0500

    resolve unit test that manages when user click in a box

--- src/components/Box.test.js
+++ src/components/Box.test.js
@@ -1,21 +1,18 @@
 import React from "react";
 import { shallow } from "enzyme";
-import TicTacToe from "./App";
 import Box from "./Box";
 
 describe("Box", () => {
   let wrapper;
-  beforeEach(
-    () =>
-      (wrapper = shallow(
-        <Box onClick={jest.fn()} value={TicTacToe.PLAYER_ONE_SYMBOL} />
-      ))
-  );
+  let mockEvent = jest.fn();
+  beforeEach(() => (wrapper = shallow(<Box onClick={mockEvent} />)));
 
   it("Should render a <button />", () => {
     expect(wrapper.find("button").length).toEqual(1);
   });
 
-  it("Should click in the box in order to bind a value", () => {
+  it("Should click in the box", () => {
+      wrapper.find('button').simulate('click');
+      expect(mockEvent.mock.calls.length).toEqual(1);
   });
 });
