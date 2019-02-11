c64ded675f079ba7ca2be7020382752392ee76cf
Erick Costa Cavalcante <erickbelfort@Ericks-MacBook-Pro.local>
2019-02-10 20:34:59 -0500

    improving css of game container

--- src/components/MovesHistory.js
+++ src/components/MovesHistory.js
@@ -2,15 +2,7 @@ import React from "react";
 import PropTypes from 'prop-types';
 
 const MovesHistory = ({ history, goTo }) => {
-  const moves = history.map((step, move) => {
-    const desc = move ? "Go to move #" + move : "Go to game start";
-    return (
-      <li key={move}>
-        <button onClick={() => goTo(move)}>{desc}</button>
-      </li>
-    );
-  });
-  return <ol>{moves}</ol>;
+  return <button onClick={() => goTo()}>undo</button>;
 };
 
 MovesHistory.propTypes = {
