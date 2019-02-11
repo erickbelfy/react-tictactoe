c7c2679512cfbdd201e7c3833d8ef0afd2800192
Erick Costa Cavalcante <erickbelfort@Ericks-MacBook-Pro.local>
2019-02-10 19:50:25 -0500

    add header unit tests

--- src/components/TurnHeader.test.js
+++ src/components/TurnHeader.test.js
@@ -11,6 +11,11 @@ describe("TurnHeader", () => {
     expect(wrapper.find("header").length).toEqual(1);
   });
 
+  it("Should check player one is the current player", () => {
+    expect(wrapper.find('.play-turn-1').length).toBe(1);
+  });
   it("Should check player two is the current player", () => {
+    wrapper.setProps({isPlayerOne: false});
+    expect(wrapper.find('.play-turn-2').length).toBe(1);
   });
 });
